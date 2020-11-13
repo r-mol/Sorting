d3ec224cd4445fa7877875502d686fce97621e29
Roman Molochkov <molochkov2017@gmail.com>
2020-11-13 17:26:42 +0300

    Insertion_Sort.cpp

---
+++ Sorting/Insertion_Sort/Insertion_Sort.cpp
@@ -0,0 +1,36 @@
+#include <iostream> 
+
+using namespace std;
+
+int main()
+{
+    int n; // Кол-во элементов
+    cin >> n;
+    int arr[1000] = {};
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+    
+    for (int i = 0; i < n; ++i)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
